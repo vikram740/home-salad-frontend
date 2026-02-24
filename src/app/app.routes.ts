913c6f940247d9587c8108b317b1865f360ef8bd
vikram740 <vikram@wiznsoft.com>
2026-02-24 12:17:12 +0530

    code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,5 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
-import { Register } from './register/register';
-import { Otp } from './otp/otp';
-import { Userdashboard } from './userdashboard/userdashboard';
-import { Adminlogin } from './adminlogin/adminlogin';
-import { Adminlogin1 } from './adminlogin1/adminlogin1';
-import { Addproduct } from './addproduct/addproduct';
-import { Userplan } from './userplan/userplan';
-import { Usereditplan } from './usereditplan/usereditplan';
-import { Userproduct } from './userproduct/userproduct';
 
 export const routes: Routes = [
  { path: '', component: Login, pathMatch: 'full' },
