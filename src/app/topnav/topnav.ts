9404f31e2b16b3c41a8f82fd531f744ecbc5e2a2
Hemalatha <hemalatha@wiznsoft.com>
2026-02-24 12:06:14 +0530

    topnavcreated

---
+++ src/app/topnav/topnav.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topnav',
+  imports: [],
+  templateUrl: './topnav.html',
+  styleUrl: './topnav.scss',
+})
+export class Topnav {
+
+}
---
+++ src/app/userorder/userorder.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-userorder',
+  imports: [],
+  templateUrl: './userorder.html',
+  styleUrl: './userorder.scss'
+})
+export class Userorder {
+
+}
